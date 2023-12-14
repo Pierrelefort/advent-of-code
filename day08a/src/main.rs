8417a0a6b25ec4af7048e0735f80cd4b1ff6ac70
Pierrelefort <pierre.lefort@epita.fr>
2023-12-14 21:07:32 +0100

    feat: day08a

--- day08a/src/main.rs
+++ day08a/src/main.rs
@@ -1,8 +1,44 @@
+use std::collections::HashMap;
+
 use misc::load_text;
 
 fn haunted_wasteland(text: &str) -> u32 {
-    return 0;
+    // Parse input
+    let instructions: &str = text.split_once("\n").unwrap().0;
+
+    let mut hashmap: HashMap<&str, (&str, &str)> = HashMap::new();
+    let lines =  text.split_once("\n\n").unwrap().1.split("\n");
+    for line in lines {
+        let (key, mut keys) = line.split_once(" = ").unwrap();
+        keys = &keys[1..keys.len() - 1];
+
+        let (key_left, key_right) = keys.split_once(", ").unwrap();
+        hashmap.insert(key, (key_left, key_right));
+    }
+
+    let mut index = "AAA";
+    let mut steps = 0;
+    let stop_key = "ZZZ";
+    while index != stop_key {
+        for instruction in instructions.chars() {
+            if instruction == 'L' {
+                index = hashmap.get_key_value(index).unwrap().1.0;
+                steps += 1;
+            }
+            else if instruction == 'R' {
+                index = hashmap.get_key_value(index).unwrap().1.1;
+                steps += 1;
+            }
+
+            if index == stop_key {
+                return steps;
+            }
+        }
+    }
+
+    return steps;
 }
+
 fn main() {
     let test_text: String = load_text(String::from("test.txt"));
     let test_value = haunted_wasteland(test_text.as_str());
