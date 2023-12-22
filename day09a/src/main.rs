22d6b808f2daec780bb0a8be60421030ccd82c5b
Pierrelefort <pierre.lefort@epita.fr>
2023-12-22 23:30:10 +0100

    feat: day09a

---
+++ day09a/src/main.rs
@@ -0,0 +1,42 @@
+use misc::load_text;
+
+fn _mirage_maintenance(line: &str) -> i64 {
+    println!("Line: {}", line);
+    let curr_vec: Vec<i64> = line.split_whitespace().map(|c| c.parse::<i64>().unwrap()).collect();
+    let last = *curr_vec.last().unwrap();
+
+    if !curr_vec.iter().all(|&i| i == last) {
+        // Create new line of difference
+        let mut str = String::from("");
+        for i in 0..curr_vec.len() - 1 {
+            let value = (curr_vec[i + 1] - curr_vec[i]).to_string();
+            str = str + " " + value.as_str();
+        }
+
+        return last + _mirage_maintenance(str.as_str());
+    }
+    return last;
+}
+
+fn mirage_maintenance(text: &str) -> i64 {
+    let lines = text.split("\n");
+    let mut res = 0;
+
+    for line in lines {
+        res = res +  _mirage_maintenance(line)
+    }
+
+
+    return res;
+}
+
+fn main() {
+    let test_text: String = load_text(String::from("test.txt"));
+    let test_value = mirage_maintenance(test_text.as_str());
+    println!("Test value: {}", test_value);
+    assert!(test_value == 114);
+    
+    let res_text: String = load_text(String::from("input.txt"));
+    let res = mirage_maintenance(res_text.as_str());
+    println!("Final: {}", res)
+}
